27b015551194517eaf280e1ad6f4ae7b22a2bc29
Krzysztof Jelski <krzysztof.jelski@pragmatists.pl>
2016-06-23 08:06:01 +0200

    Initial Commit

---
+++ FizzBuzz/FizzBuzz.h
@@ -0,0 +1,19 @@
+//
+//  FizzBuzz.h
+//  FizzBuzz
+//
+//  Created by Krzysztof Jelski on 23.06.2016.
+//  Copyright © 2016 Pragmatists. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FizzBuzz.
+FOUNDATION_EXPORT double FizzBuzzVersionNumber;
+
+//! Project version string for FizzBuzz.
+FOUNDATION_EXPORT const unsigned char FizzBuzzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FizzBuzz/PublicHeader.h>
+
+
